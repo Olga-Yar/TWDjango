d2fe14172cb9c8d51d45142b2c578cb3375896f6
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-16 23:08:55 +0300

    создание контроллеров: индекс (список сообщений), просмотр сообщения, создание сообщения и редактирование

--- main/views.py
+++ main/views.py
@@ -1,3 +1,54 @@
+from django.core.cache import cache
+from django.forms import inlineformset_factory
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from config import settings
+from main.forms import MessageForm, MailingForm
+from main.models import Message, Mailing
+
+
+class IndexView(generic.View):
+    def get(self, request):
+
+        if settings.CACHES_ENABLE:
+            key = 'message_list'
+            message_list = cache.get(key)
+            if message_list is None:
+                message_list = Message.objects.all()
+                cache.set(key, message_list)
+        else:
+            message_list = Message.objects.all()
+
+        context = {
+            'message_list': message_list,
+            'title': 'Главная'
+        }
+        return render(request, 'main/index.html', context)
+
+
+class MessageDetailView(generic.DetailView):
+    model = Message
+
+
+class MessageCreateView(generic.CreateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('main:home')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        SubjectFormset = inlineformset_factory(Message, Mailing, form=MailingForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = SubjectFormset(self.request.POST)
+        else:
+            context_data['formset'] = SubjectFormset()
+
+        return context_data
+
+
+class MessageUpdateView(generic.UpdateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('main:message_item')

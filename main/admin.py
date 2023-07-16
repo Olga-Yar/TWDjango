b1d3745976cf6d00532a1c0176a62f27c51e4662
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-16 20:47:43 +0300

    создание админки для приложения main

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Mailing, Message, LogiMail
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('mailing_time', 'periodicity', 'mailing_status', 'is_active')
+    search_fields = ('periodicity', 'is_active',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'context', 'mailing')
+    search_fields = ('title', 'mailing',)
+
+
+@admin.register(LogiMail)
+class LogiMailAdmin(admin.ModelAdmin):
+    list_display = ('date_last', 'status', 'server_answer', 'message')
+    search_fields = ('date_last', 'status',)

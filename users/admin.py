03de406a4cd8dab1ffdeba231cfa96833ac93669
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-16 20:47:55 +0300

    создание админки для приложения users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'comment', 'mailing', 'email_verify')
+    search_fields = ('email', 'first_name', 'last_name', 'mailing',)

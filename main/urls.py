958ced9b7cbe359b71296b7303193e765dd77e1b
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-17 20:28:42 +0300

    создание путей на список рассылки и логов

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import IndexView, MessageDetailView, MessageCreateView, MessageUpdateView
+from main.views import IndexView, MessageDetailView, MessageCreateView, MessageUpdateView, MailingListView, LogiListView
 
 app_name = MainConfig.name
 
@@ -10,4 +10,8 @@ urlpatterns = [
     path('message/<int:pk>/', MessageDetailView.as_view(), name='message_item'),
     path('message/create/', MessageCreateView.as_view(), name='message_create'),
     path('message/update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
+
+    path('mailing/', MailingListView.as_view(), name='mailing_list'),
+
+    path('logi/', LogiListView.as_view(), name='logi_list'),
 ]

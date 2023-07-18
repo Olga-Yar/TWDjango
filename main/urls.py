55e1a3a374e1e974ee021b0af397b54ca0439343
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-18 20:23:29 +0300

    создание urls для клиента

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import IndexView, MessageDetailView, MessageCreateView, MessageUpdateView, MailingListView, LogiListView
+from main.views import IndexView, MessageDetailView, MessageCreateView, MessageUpdateView, MailingListView, \
+    LogiListView, MessageDeleteView, ClientListView, ClientDeleteView, ClientUpdateView, ClientCreateView
 
 app_name = MainConfig.name
 
@@ -10,8 +11,14 @@ urlpatterns = [
     path('message/<int:pk>/', MessageDetailView.as_view(), name='message_item'),
     path('message/create/', MessageCreateView.as_view(), name='message_create'),
     path('message/update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
+    path('message/delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
 
     path('mailing/', MailingListView.as_view(), name='mailing_list'),
 
     path('logi/', LogiListView.as_view(), name='logi_list'),
+
+    path('client/', ClientListView.as_view(), name='client_list'),
+    path('client/create/', ClientCreateView.as_view(), name='client_create'),
+    path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
+    path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),
 ]

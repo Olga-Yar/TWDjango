5151a7bf9e974dc86b2c372ed312a794e3545bd2
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-19 20:24:23 +0300

    создание url для перехода на страницу клиента

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from main.apps import MainConfig
 from main.views import IndexView, MessageDetailView, MessageCreateView, MessageUpdateView, MailingListView, \
-    LogiListView, MessageDeleteView, ClientListView, ClientDeleteView, ClientUpdateView, ClientCreateView, MessageListView
+    LogiListView, MessageDeleteView, ClientListView, ClientDeleteView, ClientUpdateView, ClientCreateView, \
+    MessageListView, ClientDetailView
 
 app_name = MainConfig.name
 
@@ -19,6 +20,7 @@ urlpatterns = [
     path('logi/', LogiListView.as_view(), name='logi_list'),
 
     path('client/', ClientListView.as_view(), name='client_list'),
+    path('client/<int:pk>', ClientDetailView.as_view(), name='client_item'),
     path('client/create/', ClientCreateView.as_view(), name='client_create'),
     path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
     path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),

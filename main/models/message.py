165bdcf7dc87c5983289ede77477364a389dc883
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-19 20:00:23 +0300

    добавление поля создатель в модель сообщение

--- main/models/message.py
+++ main/models/message.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+from config import settings
+
 NULLABLE = {'blank': True, 'null': True}
 
 
 class Message(models.Model):
     title = models.CharField(max_length=60, verbose_name='тема письма')
     context = models.TextField(blank=True, verbose_name='тело письма')
+
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.SET_NULL, **NULLABLE,
+                                verbose_name='создатель')
 
     def __str__(self):
         return f'сообщение {self.title}'

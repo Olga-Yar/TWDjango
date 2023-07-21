9e9c03f423b76e55e261fe155b1719a0b6787780
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-22 00:20:30 +0300

    коррекция CRONJOBS

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -155,9 +155,6 @@ CACHES = {
     }
 }
 
-# CRONJOB = [
-#     ('* * * * *', 'main.services.hourly_send'),
-#     ('* * * * *', 'main.services.daily_send'),
-#     ('* * * * *', 'main.services.weekly_send'),
-#     ('* * * * *', 'main.services.monthly_send'),
-# ]
+CRONJOBS = [
+    ('*/5 * * * *', 'main.crontab_items.daily_send', '>>/tmp/scheduled_job.log'),
+]

e2d1474a47e8c6df3d7d46238d8c132c1f9e41c1
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-22 00:22:44 +0300

    корректировка urls для модели пользователи

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
@@ -15,8 +17,8 @@ urlpatterns = [
 
     path('invalid_verify/', TemplateView.as_view(template_name='users/invalid_verify.html'), name='invalid_verify'),
 
-    path('/logout', LogoutView.as_view(), name='logout'),
+    path('logout', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/genpassword/', generate_new_password, name='generate_new_password'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

bac12f5ee8c0ef1e87f70d8b632ce8cb853d4a7f
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-22 00:23:05 +0300

    корректировка urls для модели main

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from main.apps import MainConfig
 from main.views import IndexView, MessageDetailView, MessageCreateView, MessageUpdateView, MailingListView, \
@@ -8,7 +11,7 @@ from main.views import IndexView, MessageDetailView, MessageCreateView, MessageU
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='home'),  # главная страница
+    path('', cache_page(60)(IndexView.as_view()), name='home'),  # главная страница
     path('message/', MessageListView.as_view(), name='message_list'),  # список сообщений
     path('message/<int:pk>/', MessageDetailView.as_view(), name='message_item'),  # просмотр сообщения
     path('message/create/', MessageCreateView.as_view(), name='message_create'),  # создание сообщения
@@ -20,14 +23,14 @@ urlpatterns = [
     path('logi/', LogiListView.as_view(), name='logi_list'),  # список логов
 
     path('client/', ClientListView.as_view(), name='client_list'),  # список клиентов
-    path('client/<int:pk>', ClientDetailView.as_view(), name='client_item'),  # просмотр клиента
+    path('client/<int:pk>/', ClientDetailView.as_view(), name='client_item'),  # просмотр клиента
     path('client/create/', ClientCreateView.as_view(), name='client_create'),  # создание клиента
     path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),  # редактирование клиента
     path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),  # удаление клиента
 
-    path('blog/', BlogListView.as_view(), name='blog_list'),  # список статей
-    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_item'),  # просмотр статьи
+    path('blog/', cache_page(60)(BlogListView.as_view()), name='blog_list'),  # список статей
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_item'),  # просмотр статьи
     path('blog/create/', BlogCreateView.as_view(), name='blog_create'),  # создание статьи
     path('blog/update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),  # редактирование статьи
     path('blog/delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),  # удаление статьи
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
